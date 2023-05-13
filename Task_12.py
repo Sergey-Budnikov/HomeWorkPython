23314bc393d22a8331775f952952e75dfce52ee8
Будников Сергей <mr.s.b@bk.ru>
2023-05-13 23:16:13 +0300

    добавлено несколько решений задачи

--- Task_12.py
+++ Task_12.py
@@ -4,6 +4,7 @@
 Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
 Помогите Кате отгадать задуманные Петей числа.
 """
+#Решение1
 S = int(input("Сумма чисел: "))
 P = int(input("Произведение чисел: "))
 for X in range(1001):
@@ -11,6 +12,7 @@ for X in range(1001):
         if P == X*Y and S == X+Y:
             print(X,Y)
             
+#Решение2            
 """
 x = int(input())
 y = int(input())
@@ -18,6 +20,13 @@ for i in range(x):
 for j in range(y):
 if x == i + j and y == i * j:
 print(i, j)
-"""            
+"""   
+
+#Решение3
+S = int(input("Сумма чисел: "))
+P = int(input("Произведение чисел: "))
+for i in range(S):                     #либо for i in range(S//2 + 1)
+    if i * (S - i) == P:
+        print(i, S - i)        
 
               
