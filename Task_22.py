9ad6a8c1965fccc3cdb0e062724562168f18c891
Будников Сергей <mr.s.b@bk.ru>
2023-06-03 23:49:29 +0300

    Решение задач 22 и 24

--- Task_22.py
+++ Task_22.py
@@ -39,3 +39,26 @@ print(c)
 # print(c) 
 # list.sort(c)
 # print(c)
+
+def duplicate_numbers_in_the_list():
+    first_len = int(input("Введите кол-во элементов первого множества: "))
+    second_len = int(input("Введите кол-во элементов второго множества: "))
+
+    first_list = list(map(str, input("Первое множество(через пробел): ").split()))
+    second_list = list(map(str, input("Второе множество(через пробел): ").split()))
+
+    if (len(first_list) != first_len) and (len(second_list) != second_len):
+        raise Exception("Длины множеств не совпадают указанному кол-ву элементов")
+    
+    result_list = []
+    
+    for num in first_list:
+        if num in second_list:
+            result_list.append(num)
+
+    result_list.sort()
+
+    return result_list
+
+
+print(duplicate_numbers_in_the_list())
